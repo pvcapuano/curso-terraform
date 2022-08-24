9df79d7a86f61759aa3cb68b52d45c2d4be7fcad
pvcapuano <pvcapuano@gmail.com>
2022-08-24 08:43:17 -0300

    create a vpc

---
+++ aws-vpc/main.tf
@@ -0,0 +1,28 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.73.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "exemplo-terraform-cli"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+
+  default_tags {
+    tags = {
+      owner      = "pvcapuano"
+      managed-by = "terraform"
+    }
+  }
+}
