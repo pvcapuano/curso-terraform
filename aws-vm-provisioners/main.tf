2f751d5cd352f07aed028699720c402f53d9b047
Paulo Victor Capuano <pvcapuano@gmail.com>
2022-08-31 18:06:31 -0300

    provisioners

---
+++ aws-vm-provisioners/main.tf
@@ -0,0 +1,37 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.73.0"
+    }
+  }
+
+  /* backend "s3" {
+    bucket = "exemplo-terraform-cli"
+    key    = "aws-vm-provisioners/terraform.tfstate"
+    region = "eu-central-1"
+  } */
+}
+
+provider "aws" {
+  region = "eu-central-1"
+
+  default_tags {
+    tags = {
+      owner      = "pvcapuano"
+      managed-by = "terraform"
+    }
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "exemplo-terraform-cli"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
